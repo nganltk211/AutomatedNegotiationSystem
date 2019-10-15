ae0c00dcb3045c09295dac85cf7cf349374c90fa
Thi Kim Ngan Le <tle002@stud.hs-bremen.de>
2019-10-15 11:46:09 +1100

    set value on node of the line chart

--- Automated_Negotiation_System-BrokerGUI/src/gui/LineChartGUIController.java
+++ Automated_Negotiation_System-BrokerGUI/src/gui/LineChartGUIController.java
@@ -1,9 +1,18 @@
 package gui;
+
+import javafx.beans.binding.ObjectExpression;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import model.LogSession;
 import model.NegotiationLog;
 
@@ -22,10 +31,10 @@ public class LineChartGUIController {
     
     public void setNegotiation(NegotiationLog session) {
     	this.session = session;
-    	chart();
+    	drawChart();
     }
     
-    public void chart() {
+    public void drawChart() {
     	
     	x.setLabel("T");
     	y.setLabel("Price");
@@ -37,11 +46,22 @@ public class LineChartGUIController {
     	dealer.setName(session.getDealerName());
     	
     	for(LogSession x : session.getBuyerLog()) {
-    		buyer.getData().add(new XYChart.Data<String, Number>(String.valueOf(x.getStep()), x.getOffer()));
+    		Data<String, Number> n = new XYChart.Data<String, Number>(String.valueOf(x.getStep()), x.getOffer());
+    		n.setNode(createDataNode(n.YValueProperty()));
+    		buyer.getData().add(n);
     	}
     	
+    	int counter = 0;
     	for(LogSession x : session.getDealerLog()) {
-    		dealer.getData().add(new XYChart.Data<String, Number>(String.valueOf(x.getStep()), x.getOffer()));
+    		counter++;
+    		Data<String, Number> n = new XYChart.Data<String, Number>(String.valueOf(x.getStep()), x.getOffer());
+    		
+    		if (counter < session.getDealerLog().size()) {
+    			n.setNode(createDataNode(n.YValueProperty()));
+    			dealer.getData().add(n);
+    		} else {
+    			dealer.getData().add(n);
+    		}
     	}
     	
     	y.setAutoRanging(false);
@@ -51,4 +71,18 @@ public class LineChartGUIController {
     	
     	LineChart.getData().addAll(buyer, dealer);
     }
+    
+    private Node createDataNode(ObjectExpression<Number> value) {
+        Label label = new Label();
+        label.textProperty().bind(value.asString("%.0f"));
+
+        Pane pane = new Pane(label);
+        pane.setShape(new Circle(6.0));
+        pane.setScaleShape(false);
+
+        label.translateYProperty().bind(label.heightProperty().divide(-1.5));
+
+        return pane;
+    }
+    
 }
